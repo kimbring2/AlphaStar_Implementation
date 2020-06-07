5d5dfce5b2d5b085a9b0ed901451f3123ecde70d
Dohyeong Kim <kimbring2@gmail.com>
2020-06-08 04:57:17 +0900

    Delete ObserverAgent.py

--- ObserverAgent.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-class ObserverAgent():
-    def step(self, time_step, actions):
-    	#print("info:" + str(info))
-    	if (len(actions) != 0):
-    		print("actions:" + str(actions))
-        #print("{}".format(time_step.observation["game_loop"]))
