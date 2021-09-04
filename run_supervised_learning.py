c9831ad34903f3bbb7e330141c708b5b0c169b48
Dohyeong Kim <kimbring2@gmail.com>
2021-09-04 17:00:32 +0900

    Add files via upload

--- run_supervised_learning.py
+++ run_supervised_learning.py
@@ -55,17 +55,16 @@ parser.add_argument('--visualize', type=bool, default=False, help='render with p
 parser.add_argument('--training', type=bool, default=False, help='training model')
 parser.add_argument('--gpu_use', type=bool, default=False, help='use gpu')
 parser.add_argument('--seed', type=int, default=123, help='seed number')
+parser.add_argument('--training_episode', type=int, default=5000, help='training number')
 parser.add_argument('--load', type=bool, default=False, help='load pretrained model')
 parser.add_argument('--save', type=bool, default=False, help='save trained model')
 parser.add_argument('--learning_rate', type=float, default=0.0001, help='learning rate')
-parser.add_argument('--gradient_clipping', type=float, default=50.0, help='gradient clipping value')
 parser.add_argument('--player_1', type=str, default='terran', help='race of player 1')
 parser.add_argument('--player_2', type=str, default='terran', help='race of player 2')
 parser.add_argument('--screen_size', type=int, default=32, help='screen resolution')
 parser.add_argument('--minimap_size', type=int, default=32, help='minimap resolution')
 parser.add_argument('--replay_dir', type=str, default="replay", help='replay save path')
 parser.add_argument('--replay_hkl_file_path', type=str, default="replay", help='path of replay file for SL')
-parser.add_argument('--sl_training', type=bool, default=False, help='Supervised Training')
 parser.add_argument('--save_replay_episodes', type=int, default=10, help='minimap resolution')
 parser.add_argument('--tensorboard_path', type=str, default="tensorboard", help='Folder for saving Tensorboard log file')
 
@@ -195,31 +194,6 @@ env_name = arguments.environment
 workspace_path = arguments.workspace_path
 Save_Path = 'Models'
         
-if not os.path.exists(Save_Path): os.makedirs(Save_Path)
-path = '{}_A2C_{}_{}_{}'.format(env_name, seed, arguments.learning_rate, arguments.gradient_clipping)
-scores, episodes, average = [], [], []
-def PlotModel(score, episode):
-    fig = plt.figure(figsize=(18,9))
-    ax = fig.add_subplot(111)
-    ax.locator_params(numticks=12)
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-    scores.append(score)
-    episodes.append(episode)
-    average.append(sum(scores[-50:]) / len(scores[-50:]))
-    if True:
-        ax.plot(episodes, scores, 'b')
-        ax.plot(episodes, average, 'r')
-        ax.set_ylabel('Score', fontsize=18)
-        ax.set_xlabel('Steps', fontsize=18)
-        try:
-            fig.savefig(path +  ".png")
-            plt.close(fig)
-        except OSError:
-            pass
-
-    return average[-1]
-
 model = network.make_model('alphastar')
 home_agent = agent.A2CAgent(model, arguments.learning_rate, arguments.gradient_clipping)
 
@@ -584,9 +558,6 @@ def supervised_replay(replay_feature_screen_list, replay_feature_minimap_list,
 
 
 def supervised_train(dataset, training_episode):
-    # Initialization
-    #home_agent.load(workspace_path + '/Models/supervised_model')
-
     def iter_dataset(_dataset):
         dataset_iterator = iter(_dataset)
         while True:
@@ -651,229 +622,8 @@ def supervised_train(dataset, training_episode):
             home_agent.save(workspace_path + '/Models/supervised_model_' + str(training_step / 10000))
 
 
-feature_screen_size = arguments.screen_size
-feature_minimap_size = arguments.minimap_size
-rgb_screen_size = None
-rgb_minimap_size = None
-action_space = None
-use_feature_units = True
-use_raw_units = False
-step_mul = 4
-game_steps_per_episode = None
-disable_fog = False
-
-minigame_environment_list = ['MoveToBeacon', 'DefeatRoaches', 'BuildMarines']
-
-if arguments.environment not in minigame_environment_list:
-  players = [sc2_env.Agent(sc2_env.Race[arguments.player_1]), sc2_env.Bot(sc2_env.Race[arguments.player_2], sc2_env.Difficulty.very_easy)]
-else:
-  players = [sc2_env.Agent(sc2_env.Race[arguments.player_1])]
-
-env = sc2_env.SC2Env(
-      map_name=env_name,
-      players=players,
-      agent_interface_format=sc2_env.parse_agent_interface_format(
-        feature_screen=feature_screen_size,
-        feature_minimap=feature_minimap_size,
-        rgb_screen=rgb_screen_size,
-        rgb_minimap=rgb_minimap_size,
-        action_space=action_space,
-        use_feature_units=use_feature_units),
-      step_mul=step_mul,
-      game_steps_per_episode=game_steps_per_episode,
-      disable_fog=disable_fog,
-      visualize=arguments.visualize,
-      replay_dir=arguments.replay_dir,
-      save_replay_episodes=arguments.save_replay_episodes)
-
-
-def reinforcement_train(training_episode):
-    score_list = []
-    EPISODES, episode, max_average, SAVING  = 20000, 0, 5.0, ''
-  
-    if arguments.load != False:
-        #home_agent.load(workspace_path + '/Models/BuildMarines/reinforcment_model')
-        home_agent.load(workspace_path + '/Models/supervised_model_70.5')
-
-    while episode < training_episode:
-        # Reset episode
-        home_score, home_done, SAVING = 0, False, ''
-        opponent_score, opponent_done = 0, False
-        state = env.reset()
-
-        home_feature_screen_list, home_feature_minimap_list = [], []
-        home_feature_player_list, home_feature_units_list = [], []
-        home_available_actions_list, last_action_type_list = [], []
-        home_fn_id_list, home_arg_ids_list, home_rewards, home_dones = [], [], [], []
-        home_memory_state_list, home_carry_state_list = [], []
-        game_loop_list = []
-        home_build_queue_list, home_single_select_list = [], []
-        home_multi_select_list, home_score_cumulative_list = [], []
-
-        memory_state = np.zeros([1,256], dtype=np.float32)
-        carry_state =  np.zeros([1,256], dtype=np.float32)
-
-        last_action_type = [0]
-        initial_memory_state = memory_state
-        initial_carry_state = carry_state
-        while not home_done:
-            home_state = state[0]
-            #print("home_state[3]: {}".format(home_state[3]))
-            #print("home_state[3]['feature_units']: {}".format(home_state[3]['feature_units']))
-            game_loop = home_state[3]['game_loop']
-
-            home_feature_screen = home_state[3]['feature_screen']
-            home_feature_screen = utils.preprocess_screen(home_feature_screen)
-            home_feature_screen = np.transpose(home_feature_screen, (1, 2, 0))
-
-            home_player = home_state[3]['player']
-            home_player = utils.preprocess_player(home_player)
-
-            home_feature_minimap = home_state[3]['feature_minimap']
-            home_feature_minimap = utils.preprocess_minimap(home_feature_minimap)
-            home_feature_minimap = np.transpose(home_feature_minimap, (1, 2, 0))
-
-            home_available_actions = home_state[3]['available_actions']
-            home_available_actions = utils.preprocess_available_actions(home_available_actions)
-
-            home_feature_units = home_state[3]['feature_units']
-            home_feature_units = utils.preprocess_feature_units(home_feature_units, feature_screen_size)
-
-            home_build_queue = home_state[3]['build_queue']
-            home_build_queue = utils.preprocess_build_queue(home_build_queue)
-
-            home_single_select = home_state[3]['single_select']
-            home_single_select = utils.preprocess_single_select(home_single_select)
-
-            home_multi_select = home_state[3]['multi_select']
-            home_multi_select = utils.preprocess_multi_select(home_multi_select)
-
-            home_score_cumulative = home_state[3]['score_cumulative']
-            home_score_cumulative = utils.preprocess_score_cumulative(home_score_cumulative)
-
-            home_feature_screen_array = np.array([home_feature_screen])
-            home_feature_minimap_array = np.array([home_feature_minimap])
-            home_player_array = np.array([home_player])
-            home_feature_units_array = np.array([home_feature_units])
-            home_available_actions_array = np.array([home_available_actions])
-            game_loop_array = np.array([game_loop])
-            last_action_type_array = np.array([last_action_type])
-            home_build_queue_array = np.array([home_build_queue])
-            home_single_select_array = np.array([home_single_select])
-            home_multi_select_array = np.array([home_multi_select])
-            home_score_cumulative_array = np.array([home_score_cumulative])
-
-            home_feature_screen_list.append(home_feature_screen_array)
-            home_feature_minimap_list.append(home_feature_minimap_array)
-            home_player_list.append(home_player_array)
-            home_feature_units_list.append(home_feature_units_array)
-            home_available_actions_list.append([home_available_actions])
-            home_memory_state_list.append(memory_state)
-            home_carry_state_list.append(carry_state)
-            game_loop_list.append(game_loop_array)
-            last_action_type_list.append(np.array([last_action_type]))
-            home_build_queue_list.append(home_build_queue_array)
-            home_single_select_list.append(home_single_select_array)
-            home_multi_select_list.append(home_multi_select_array)
-            home_score_cumulative_list.append(home_score_cumulative_array)
-
-            home_prediction = home_agent.act(home_feature_screen_array, home_feature_minimap_array,
-                                                    home_player_array, home_feature_units_array, 
-                                                    home_available_actions_array, 
-                                                    memory_state, carry_state,
-                                                    game_loop_array, last_action_type_array,
-                                                    home_build_queue_array, home_single_select_array, 
-                                                    home_multi_select_array, home_score_cumulative_array)
-            home_fn_pi = home_prediction['fn_out']
-            home_arg_pis = home_prediction['args_out']
-            home_next_memory_state = home_prediction['final_memory_state']
-            home_next_carry_state = home_prediction['final_carry_state']
-
-            home_fn_samples, home_arg_samples = sample_actions(home_available_actions, home_fn_pi, home_arg_pis)
-            home_fn_id, home_arg_ids = mask_unused_argument_samples(home_fn_samples, home_arg_samples)
-            home_fn_id_list.append(home_fn_id[0])
-
-            home_arg_id_list = []
-            for arg_type in home_arg_ids.keys():
-                arg_id = home_arg_ids[arg_type]
-                home_arg_id_list.append(arg_id)
-
-            home_arg_ids_list.append(np.array([home_arg_id_list]))
-            home_actions_list = actions_to_pysc2(home_fn_id, home_arg_ids, (32, 32))
-            last_action_type = home_fn_id
-
-            actions_list = [home_actions_list]
-            #actions_list = [actions.FUNCTIONS.no_op()]
-            next_state = env.step(actions_list)
-            #print("actions_list: {}".format(actions_list))
-            home_next_state = next_state[0]
-            home_feature_next_screen = home_next_state[3]['feature_screen']
-            home_feature_next_screen = utils.preprocess_screen(home_feature_next_screen)
-            home_feature_next_screen = np.transpose(home_feature_next_screen, (1, 2, 0))
-            home_feature_next_screen_array = np.array([home_feature_next_screen])
-
-            home_done = home_next_state[0]
-            if home_done == StepType.LAST:
-                home_done = True
-            else:
-                home_done = False
-
-            state = next_state
-            memory_state = home_next_memory_state
-            carry_state =  home_next_carry_state
-
-            home_reward = float(home_next_state[1])
-            home_rewards.append(home_reward)
-            home_dones.append(home_done)
-
-            home_score += home_reward
-            if len(home_feature_screen_list) == 16:
-                if arguments.training == True:
-                  #print("len(last_action_type_list): ", len(last_action_type_list))
-                  home_agent.reinforcement_replay(home_feature_screen_list, home_feature_minimap_list,
-                                                          home_player_list, home_feature_units_list, 
-                                                          home_available_actions_list, home_fn_id_list, home_arg_ids_list, 
-                                                          home_rewards, home_dones, 
-                                                          initial_memory_state, initial_carry_state,
-                                                          game_loop_list, last_action_type_list,
-                                                          home_build_queue_list, home_single_select_list,
-                                                          home_multi_select_list, home_score_cumulative_list)
-
-                home_feature_screen_list, home_feature_minimap_list = [], []
-                home_player_list, home_feature_units_list = [], []
-                home_available_actions_list, last_action_type_list = [], []
-                home_fn_id_list, home_arg_ids_list, home_rewards, home_dones = [], [], [], []
-                home_memory_state_list, home_carry_state_list = [], []
-                game_loop_list = []
-                home_build_queue_list, home_single_select_list = [], []
-                home_multi_select_list, home_score_cumulative_list = [], []
-
-                initial_memory_state = memory_state
-                initial_carry_state =  carry_state
-
-        score_list.append(home_score)
-        average = sum(score_list) / len(score_list)
-        if average >= max_average:
-          SAVING = "SAVING"
-          max_average = average
-          home_agent.save(workspace_path + '/Models/reinforcment_model')
-        else:
-          SAVING = ""
-
-        PlotModel(home_score, episode)
-        print("episode: {}/{}, score: {}, average: {:.2f} {}".format(episode, EPISODES, home_score, average, SAVING))
-        if episode < EPISODES:
-            episode += 1
-
-    env.close()   
-
-
 def main():
-  if arguments.sl_training:
-    supervised_train(dataset, 5000)
-    reinforcement_train(20000)
-  else:
-    reinforcement_train(10000)
+  supervised_train(dataset, arguments.training_episode)
 
 
 if __name__ == "__main__":
