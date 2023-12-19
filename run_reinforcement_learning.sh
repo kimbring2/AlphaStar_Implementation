def9e38f1eb3c5ba9ee7261795cb075a8a238caa
Dohyeong Kim <kimbring2@gmail.com>
2023-12-19 20:44:59 +0900

    Update run_reinforcement_learning.sh

--- run_reinforcement_learning.sh
+++ run_reinforcement_learning.sh
@@ -7,7 +7,7 @@ GRADIENT_CLIPPING=$5
 tmux new-session -d -t impala_pysc2
 
 tmux new-window -d -n learner
-COMMAND_LEARNER='python3.8 learner.py --env_num '"${NUM_ACTORS}"' --gpu_use '"${GPU_USE}"' --model_name '"${MODEL_NAME}"'  --gradient_clipping '"${GRADIENT_CLIPPING}"''
+COMMAND_LEARNER='python learner.py --env_num '"${NUM_ACTORS}"' --gpu_use '"${GPU_USE}"' --model_name '"${MODEL_NAME}"'  --gradient_clipping '"${GRADIENT_CLIPPING}"''
 echo $COMMAND_LEARNER
 
 tmux send-keys -t "learner" "$COMMAND_LEARNER" ENTER
@@ -16,7 +16,7 @@ sleep 8.0
 
 for ((id=0; id < $NUM_ACTORS; id++)); do
     tmux new-window -d -n "actor_${id}"
-    COMMAND='python3.8 actor.py --env_id '"${id}"' --environment '"${ENVIRONMNET}"''
+    COMMAND='python actor.py --env_id '"${id}"' --environment '"${ENVIRONMNET}"''
     tmux send-keys -t "actor_${id}" "$COMMAND" ENTER
 
     sleep 2.0
